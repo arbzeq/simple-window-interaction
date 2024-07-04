343dbb2c78fde138a46b3f53dcfadcfb795c76a3
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-07-04 17:36:31 +0200

    Done

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ console.log(`Hej ${yourName}`);
 
 
 //Använd prompt för att fråga användaren efter deras födelseår. Spara detta i en variabel.
-const yourBirthYear = parseInt(prompt("Skriv in ditt födelseår!"));
+let yourBirthDate = prompt("Skriv in ditt födelsedatum!");
+yourBirthDate = Date.parse(yourBirthDate);
 
-let currentYear = Date().getFullYear();
-
-let currentAge = currentYear - yourBirthYear;
+let currentAge = Math.floor((Date.now() - yourBirthDate) / 31536000000);
 
 console.log(`Du är ${currentAge} år gammal.`);
+
 
 let number1 = parseInt(prompt("Skriv in ett nummer"));
 let number2 = parseInt(prompt("Skriv in ett annat nummer"));
@@ -20,4 +20,4 @@ console.log(`Subtraktionen är ${number1 - number2}`);
 console.log(`Produkten är ${number1 * number2}`);
 console.log(`Kvoten är ${number1 / number2}`);
 
-console.log(`Kvoten är ${number1 / number2}`);
+alert("Beräkningarna är klara. Kolla consollen för info.");
